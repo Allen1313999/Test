86a3e77712fece15374b5c3a523d75472a0dc9e8
allen <tiyang.chang@ue-germany.de>
2022-06-23 20:21:27 +0200

    test

--- Test/src/Test/Test.java
+++ Test/src/Test/Test.java
@@ -4,11 +4,16 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
 		
-		System.out.println("test2");
+		int a, b;
+		double c;
 		
-		System.out.println("test3");
+		a = 20;
+		b = 15;
+		c = a / b;
+		
+		
+		System.out.println(c);
 
 	}
 
